10757b652f0fc310dac1560091c7067244b66286
Cristian Widenhouse <cgwidenhouse@gmail.com>
2020-12-28 14:29:56 -0500

    remove the card class as it's now a component

--- src/app/card.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Card } from './card';
-import { Suit } from './suit.enum';
-
-describe('Card', () => {
-  it('should create an instance', () => {
-    expect(new Card(Suit.Spades, 1)).toBeTruthy();
-  });
-});
